35ba1ff657fd7f8e26c3777dc3a08541e3ba88cb
Eric C. Anderson <eric.anderson@noaa.gov>
2025-03-13 11:44:20 -0600

    maintenance

--- development/scratch-timings-etc/scratch-timings-compare.R
+++ development/scratch-timings-etc/scratch-timings-compare.R
@@ -9,17 +9,23 @@ times <- list(
 ) %>%
   bind_rows(.id = "file_system") %>%
   select(-`h:m:s`) %>%
-  pivot_longer(cols = s:cpu_time) %>%
+  rename(wall_time_seconds = s) %>%
+  pivot_longer(cols = wall_time_seconds:cpu_time) %>%
   separate(file, into = c("rule", "sample"), sep = "/") %>%
   pivot_wider(
     names_from = file_system,
     values_from = value
   )
 
-# now plot those, facet-gridding on rule and name
-times %>%
-  filter(name == "mean_load") %>%
+plot_it <- function(X) {
+  X %>%
   ggplot(aes(x = nfs, y = scratch, fill = rule)) +
   geom_point(shape = 21, stroke = 0.1) + 
-  facet_grid(rule ~ name, scales = "free") +
+  facet_grid(rule ~ name, scales = "free_x") +
   geom_abline(slope = 1, intercept = 0)
+}
+
+# now plot those, facet-gridding on rule and name
+times %>%
+  filter(name == "wall_time_seconds", rule %in% c("fastqc_read1", "fastqc_read2")) %>%
+  plot_it()
